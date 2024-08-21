9b3df1dc54a799a6b0518f8e97aa6c0ca9caf6f4
LLynx31 <christian.sagoe119@gmail.com>
2024-08-21 17:14:20 +0000

    correction ui

--- app/m/categorie/[code]/page.jsx
+++ app/m/categorie/[code]/page.jsx
@@ -1,152 +1,178 @@
-'use client'
+"use client";
 
-import {Categories, SousCategories} from "@/app/ui/categorie";
+import { Categories, SousCategories } from "@/app/ui/categorie";
 import Article from "@/app/ui/article";
 import { useState, useRef, useEffect, useContext } from "react";
 import LoadingSpinner from "@/app/ui/loading";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 import { PageContext } from "@/app/contextProvider";
 
-
-export default function PageCategorie({params}){
-
+export default function PageCategorie({ params }) {
   const thisPage = useContext(PageContext);
   useEffect(() => {
     thisPage.setPage(false);
   });
 
-  const [categorieInView, setCategorieInView] = useState('best seller')
-  const [nameCetgorie, setNameCetgorie] = useState('best seller')
+  const [categorieInView, setCategorieInView] = useState("best seller");
+  const [nameCetgorie, setNameCetgorie] = useState("best seller");
 
-  const refBestSeller = useRef(null)
-  const refArrivage = useRef(null)
-  
+  const refBestSeller = useRef(null);
+  const refArrivage = useRef(null);
 
-  useEffect(()=>{
-
+  useEffect(() => {
     const observerBestSeller = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
+      entries.forEach((entry) => {
         if (entry.isIntersecting) {
           // L'élément est en vue
-          setCategorieInView('best seller')
+          setCategorieInView("best seller");
         }
       });
     });
-  
+
     const observerArrival = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
+      entries.forEach((entry) => {
         if (entry.isIntersecting) {
           // L'élément est en vue
-          setCategorieInView('arrivage')
-        } 
+          setCategorieInView("arrivage");
+        }
       });
     });
-  
-    observerBestSeller.observe(refBestSeller.current)
-    observerArrival.observe(refArrivage.current)
 
+    observerBestSeller.observe(refBestSeller.current);
+    observerArrival.observe(refArrivage.current);
+  });
 
-  })
+  const [dataSousCategorie, setDataSousCategorie] = useState(null);
+  const [dataProductSousCategorie, setDataProductSousCategorie] =
+    useState(null);
 
+  useEffect(() => {
+    fetch(
+      "https://api.3dsupplychains.com/api/categories?page=1&code=" + params.code
+    )
+      .then((response) => response.json())
+      .then((responseParse) => {
+        if (responseParse["hydra:member"][0]) {
+          setDataSousCategorie(responseParse["hydra:member"][0].sousCategories);
+          setNameCetgorie(responseParse["hydra:member"][0].libelle);
+        }
 
-  const [dataSousCategorie, setDataSousCategorie] = useState(null)
-  const [dataProductSousCategorie, setDataProductSousCategorie] = useState(null)
+        fetch(
+          "https://api.3dsupplychains.com/api/produits?page=1&sousCategorie=" +
+            responseParse["hydra:member"][0].sousCategories[0]["@id"]
+        )
+          .then((response) => response.json())
+          .then((responseParse) =>
+            setDataProductSousCategorie(responseParse["hydra:member"])
+          )
+          .catch((error) => console.error(error));
+      });
+  }, []);
 
-  useEffect(()=>{
-      fetch("https://api.3dsupplychains.com/api/categories?page=1&code=" + params.code)
-      .then((response)=>response.json())
-      .then((responseParse) =>
-        {
-          
-          if(responseParse["hydra:member"][0]) {
-            setDataSousCategorie(responseParse["hydra:member"][0].sousCategories)
-            setNameCetgorie(responseParse["hydra:member"][0].libelle)
-          } 
-          
-          fetch("https://api.3dsupplychains.com/api/produits?page=1&sousCategorie=" + responseParse["hydra:member"][0].sousCategories[0]["@id"])
-          .then((response)=>response.json())
-          .then((responseParse) => setDataProductSousCategorie(responseParse["hydra:member"]))
-          .catch((error)=> console.error(error))
-        })}
-       
-    , [])
+  if (dataSousCategorie && dataProductSousCategorie) {
+    const listDataProduct = dataProductSousCategorie.map((product) => (
+      <Article
+        poids={product.description2}
+        code={product.id}
+        key={product["@id"]}
+        nom={product.nom}
+        image={product.imageProduits[0]?.path}
+        price={product.price}
+        newPrice={product.newPrice}
+      ></Article>
+    ));
 
-  
-  if (dataSousCategorie && dataProductSousCategorie){
-    
-    const listDataProduct = dataProductSousCategorie.map(product => <Article poids={product.description2} code={product.id} key={product["@id"]} nom={product.nom} image={product.imageProduits[0]?.path} price={product.price} newPrice={product.newPrice}></Article>)
-
-
-  return (
-    <motion.div initial={{opacity: 0, y:50}} animate={{opacity:1, y:0}} transition={{duration:0.3}}>
-
-      <div className="text-xl mt-5 sm:mt-1 flex flex-col justify-center items-center bg-amber-200 h-[200px] border-b border-b-gray-300">
-        <div className="text-[13px] tracking-[.25em] ">Catégorie</div>
-        <div className="font-bold">{nameCetgorie}</div>
-      </div>
-
-
-      <div className="px-3 sm:px-8 pt-5">
-        <Categories></Categories>
-      {/* Categorie */}
-      <SousCategories data={dataSousCategorie} setDataProduct={setDataProductSousCategorie} code={params.code} ></SousCategories>
-
-      <div className="pt-1 gap-9 mt-8 flex">
-
-        {/* barre de navigation */}
-        <div className="sticky hidden sm:block top-5 h-fit items-stretch content-end flex-wrap max-w-[202px] flex-col ">
-            <div className="text-black text-xl font-bold w-full">
-              Navigation
-            </div>
-            <div className="items-center flex gap-1.5 mt-2.5 px-2">
-              <div className={categorieInView == 'best seller' ? 'bg-rouge flex w-[8px] shrink-0 h-[8px]  flex-col my-auto rounded-[50%]' : 'bg-white flex w-[8px] shrink-0 h-[8px]  flex-col my-auto rounded-[50%]'} />
-              <div className="text-black text-base font-medium self-stretch">
-                Best sellers
-              </div>
-            </div>
-            <div className="items-center flex gap-1.5 mt-2.5 px-2">
-              <div  className={categorieInView == 'arrivage' ? 'bg-rouge flex w-[8px] shrink-0 h-[8px]  flex-col my-auto rounded-[50%]' : 'bg-white flex w-[8px] shrink-0 h-[8px]  flex-col my-auto rounded-[50%]'} />
-              <div className="text-black text-base font-medium self-stretch">
-              Nouveaux arrivages
-              </div>
-            </div>
-            
+    return (
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.3 }}
+      >
+        <div className="text-xl mt-5 sm:mt-1 flex flex-col justify-center items-center bg-amber-200 h-[200px] border-b border-b-gray-300">
+          <div className="text-[13px] tracking-[.25em] ">Catégorie</div>
+          <div className="font-bold">{nameCetgorie}</div>
         </div>
 
+        <div className="px-3 sm:px-8 pt-5">
+          <Categories></Categories>
+          {/* Categorie */}
+          <SousCategories
+            data={dataSousCategorie}
+            setDataProduct={setDataProductSousCategorie}
+            code={params.code}
+            categorie={nameCetgorie}
+          ></SousCategories>
 
-        {/* ventes */}
-        <div className="flex flex-col gap-5">
-
-          {/* produit en promotion */}
-          <div  className="mb-16">
-              <h1 ref={refBestSeller} className="text-xl font-bold ">Les best sellers</h1>
-              <p className="text-base ">Les produits les plus vendus de la plateforme</p>
-              <div  className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-5 mt-5"> 
-                {listDataProduct}
-           
+          <div className="pt-1 gap-9 mt-8 flex">
+            {/* barre de navigation */}
+            <div className="sticky hidden sm:block top-5 h-fit items-stretch content-end flex-wrap max-w-[202px] flex-col ">
+              <div className="text-black text-xl font-bold w-full">
+                Navigation
+              </div>
+              <div className="items-center flex gap-1.5 mt-2.5 px-2">
+                <div
+                  className={
+                    categorieInView == "best seller"
+                      ? "bg-rouge flex w-[8px] shrink-0 h-[8px]  flex-col my-auto rounded-[50%]"
+                      : "bg-white flex w-[8px] shrink-0 h-[8px]  flex-col my-auto rounded-[50%]"
+                  }
+                />
+                <div className="text-black text-base font-medium self-stretch">
+                  Best sellers
+                </div>
+              </div>
+              <div className="items-center flex gap-1.5 mt-2.5 px-2">
+                <div
+                  className={
+                    categorieInView == "arrivage"
+                      ? "bg-rouge flex w-[8px] shrink-0 h-[8px]  flex-col my-auto rounded-[50%]"
+                      : "bg-white flex w-[8px] shrink-0 h-[8px]  flex-col my-auto rounded-[50%]"
+                  }
+                />
+                <div className="text-black text-base font-medium self-stretch">
+                  Nouveaux arrivages
+                </div>
               </div>
             </div>
-        
-          {/* nouveau arrivage*/}
-          <div  className="mb-16">
-              <h1 ref={refArrivage} className="text-xl font-bold ">Nouveaux arrivages</h1>
-              <p className="text-base ">Les produits qui viennent d'arriver en stock</p>
-              <div  className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-5 mt-5"> 
-              {listDataProduct}
+
+            {/* ventes */}
+            <div className="flex flex-col gap-5">
+              {/* produit en promotion */}
+              <div className="mb-16">
+                <h1 ref={refBestSeller} className="text-xl font-bold ">
+                  Les best sellers
+                </h1>
+                <p className="text-base ">
+                  Les produits les plus vendus de la plateforme
+                </p>
+                <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-5 mt-5">
+                  {listDataProduct}
+                </div>
+              </div>
+
+              {/* nouveau arrivage*/}
+              <div className="mb-16">
+                <h1 ref={refArrivage} className="text-xl font-bold ">
+                  Nouveaux arrivages
+                </h1>
+                <p className="text-base ">
+                  Les produits qui viennent d'arriver en stock
+                </p>
+                <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-5 mt-5">
+                  {listDataProduct}
+                </div>
               </div>
             </div>
-
-
-            
-          
+          </div>
         </div>
-
-      </div>
-    </div>
-    </motion.div>
-    
-  )
-} else {
-  return <><div ref={refArrivage}></div> <div ref={refBestSeller}></div><LoadingSpinner></LoadingSpinner></>
-}
+      </motion.div>
+    );
+  } else {
+    return (
+      <>
+        <div ref={refArrivage}></div> <div ref={refBestSeller}></div>
+        <LoadingSpinner></LoadingSpinner>
+      </>
+    );
+  }
 }
--- app/ui/categorie.jsx
+++ app/ui/categorie.jsx
@@ -1,120 +1,143 @@
-'use client'
+"use client";
 
-import Link from 'next/link';
-import { register } from 'swiper/element/bundle';
-import { useEffect, useState } from 'react';
-import {motion} from "framer-motion"
+import Link from "next/link";
+import { register } from "swiper/element/bundle";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
-register()
+register();
 
-function Categorie({code, image, nom, className}){
-
-    return (
-      <motion.div whileHover={{scale:0.9}}>
-          <Link href={"/m/categorie/" + code}>
-          <div className={"flex-col w-[100px] flex sm:w-fit h-max" + className}>
+function Categorie({ code, image, nom, className }) {
+  return (
+    <motion.div whileHover={{ scale: 0.9 }}>
+      <Link href={"/m/categorie/" + code}>
+        <div className={"flex-col flex sm:w-fit h-max" + className}>
           <img
             loading="lazy"
-            srcSet={image ? "https://api.3dsupplychains.com/" + image : "/image_categorie.jpg"}
-            className=" sm:w-[120px] "
+            srcSet={
+              image
+                ? "https://api.3dsupplychains.com/" + image
+                : "/image_categorie.jpg"
+            }
+            className=""
           />
-          <div className="text-black overflow-hidden text-wrap   text-[11px] whitespace-nowrap bg-amber-300 justify-center py-1">
+          <div className="text-black overflow-hidden text-wrap text-center  text-[11px] whitespace-nowrap bg-amber-300 justify-center py-1">
             {nom}
           </div>
-          </div>
-        </Link>
-      </motion.div>
-        
-      )
+        </div>
+      </Link>
+    </motion.div>
+  );
 }
 
-
-function SousCategorie({id, image, nom, className, setDataProduct}){
-
-  async function getProduct(){
-    setDataProduct(null)
-    fetch("https://api.3dsupplychains.com/api/produits?page=1&sousCategorie=" + id)
-          .then((response)=>response.json())
-          .then((responseParse) => setDataProduct(responseParse["hydra:member"]))
-          .catch((error)=> console.error(error))
+function SousCategorie({ id, image, nom, className, setDataProduct }) {
+  async function getProduct() {
+    setDataProduct(null);
+    fetch(
+      "https://api.3dsupplychains.com/api/produits?page=1&sousCategorie=" + id
+    )
+      .then((response) => response.json())
+      .then((responseParse) => setDataProduct(responseParse["hydra:member"]))
+      .catch((error) => console.error(error));
   }
 
   return (
-      
-        <motion.div whileHover={{scale:0.9}} className={"flex-col flex w-fit cursor-pointer " + className} onClick={()=>getProduct()}>
-        <img
-          loading="lazy"
-          srcSet={image ? "https://api.3dsupplychains.com/" + image : "/image_categorie.jpg"}
-          className="w-[120px] sm:w-full"
-        />
-        <div className="text-black overflow-hidden text-[11px] whitespace-nowrap bg-amber-300 justify-center py-1">
-          {nom}
-        </div>
-        </motion.div>
-    )
-}
-
-export function Categories(){
-
-  const [data, setData] = useState([])
-
-    useEffect(()=>{
-      fetch("https://api.3dsupplychains.com/api/categories")
-      .then((response)=>response.json())
-      .then((responseParse)=>setData(responseParse["hydra:member"]))
-    }, [])
-
-    let listCategorie
-    if(data.length > 0){
-      listCategorie = data.map(categorie =>
-        <swiper-slide key={categorie.code}>
-          <Categorie code={categorie.code} image={categorie.coverImagePath} nom={categorie.libelle}></Categorie>
-        </swiper-slide>
-      )
-
-    }
-
-
-    return(
-        <motion.div initial={{opacity:0, y:50}} whileInView={{opacity:1, y:0}} transition={{duration:0.3,delay:0.2}}><div className='my-5 py-5 md:block hidden'>
-        {/*<swiper-container space-between="10" slides-per-view={"2"} navigation="true" speed="500" css-mode="true">
-          {listCategorie}
-    </swiper-container>*/}
-          <div className='flex gap-3'>
-            {listCategorie}
-          </div>
+    <motion.div
+      whileHover={{ scale: 0.9 }}
+      className={"flex-col flex w-fit cursor-pointer " + className}
+      onClick={() => getProduct()}
+    >
+      <img
+        loading="lazy"
+        srcSet={
+          image
+            ? "https://api.3dsupplychains.com/" + image
+            : "/image_categorie.jpg"
+        }
+        className="w-[120px] sm:w-full"
+      />
+      <div className="text-black overflow-hidden text-[11px] whitespace-nowrap bg-amber-300 justify-center py-1">
+        {nom}
       </div>
-      <div className='my-5 md:hidden'>
-          <div className='flex gap-3'>
-            {listCategorie}
-          </div>
-        </div></motion.div>
-        
-    )
+    </motion.div>
+  );
 }
 
+export function Categories() {
+  const [data, setData] = useState([]);
 
-export function SousCategories({code,data,setDataProduct}){
-      console.log(data)
-      const listCategorie = data.map(categorie =>
-        <swiper-slide key={categorie.code}>
-          <SousCategorie id={categorie["@id"]} setDataProduct={setDataProduct} image={categorie.coverImagePath} nom={categorie.libelle}></SousCategorie>
-        </swiper-slide>
-      )
+  useEffect(() => {
+    fetch("https://api.3dsupplychains.com/api/categories")
+      .then((response) => response.json())
+      .then((responseParse) => setData(responseParse["hydra:member"]));
+  }, []);
 
-    return(
-        <motion.div initial={{opacity:0, y:50}} whileInView={{opacity:1, y:0}} transition={{duration:0.3}}><div className='my-5 md:block hidden'>
-          <div className='text-base'>Sous categorie {}</div>
-        <swiper-container space-between="10" slides-per-view={"9"} navigation="true" speed="500" css-mode="true">
+  let listCategorie;
+  if (data.length > 0) {
+    listCategorie = data.map((categorie) => (
+      <Categorie
+        key={categorie.code}
+        code={categorie.code}
+        image={categorie.coverImagePath}
+        nom={categorie.libelle}
+      ></Categorie>
+    ));
+  }
+
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.3, delay: 0.2 }}
+    >
+      <div className="text-base">Categories {}</div>
+      <hr className="my-2"></hr>
+      {/*<div className="my-5 py-5 md:block hidden">
+        {<swiper-container space-between="10" slides-per-view={"2"} navigation="true" speed="500" css-mode="true">
           {listCategorie}
-          
-        </swiper-container>
-      </div><div className='my-5 md:hidden'>
-        <div className='grid grid-cols-4 gap-2'>
-            {listCategorie}
-          </div>
-        </div></motion.div>
-        
-    )
+    </swiper-container>}
+        <div className="grid grid-cols-4 gap-3">{listCategorie}</div>
+      </div>*/}
+      <div className="my-5 ">
+        <div className="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 gap-3">
+          {listCategorie}
+        </div>
+      </div>
+    </motion.div>
+  );
 }
 
+export function SousCategories({data, setDataProduct, categorie }) {
+  //console.log(data);
+  const listCategorie = data.map((categorie) => (
+    <SousCategorie
+      key={categorie.code}
+      id={categorie["@id"]}
+      setDataProduct={setDataProduct}
+      image={categorie.coverImagePath}
+      nom={categorie.libelle}
+    ></SousCategorie>
+  ));
+
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.3, delay: 0.2 }}
+    >
+      <div className="text-base">Sous Categories de "{categorie}"</div>
+      <hr className="my-2"></hr>
+      {/*<div className="my-5 py-5 md:block hidden">
+        {<swiper-container space-between="10" slides-per-view={"2"} navigation="true" speed="500" css-mode="true">
+          {listCategorie}
+    </swiper-container>}
+        <div className="grid grid-cols-4 gap-3">{listCategorie}</div>
+      </div>*/}
+      <div className="my-5 ">
+        <div className="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 gap-3">
+          {listCategorie}
+        </div>
+      </div>
+    </motion.div>
+  );
+}
